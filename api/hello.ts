a3f378aa8235030f6fbbfa9603678c28a5b1ad2c
Ohta Akira <waterlow3442@gmail.com>
2022-07-13 22:50:08 +0900

    画像に文字入れ

--- api/hello.ts
+++ api/hello.ts
@@ -1,18 +1,10 @@
-// import path from 'path';
-// import fs from 'fs';
-import { createCanvas } from 'canvas';
+import { createCanvas, registerFont } from 'canvas';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function (req: VercelRequest, res: VercelResponse) {
-  // const filePath = path.join(process.cwd(), `/api/150.jpeg`);
-  // const imageBuffer = fs.readFileSync(filePath);
-  const width = 1200;
-  const height = 627;
-
-  // Add post object with the content to render
-  const post = {
-    title: "Draw and save images with Canvas and Node"
-  }
+  registerFont('./api/NotoSansJP-Bold.otf', { family: 'NotoSansJP' });
+  const width = 600;
+  const height = 381;
 
   const canvas = createCanvas(width, height);
   const context = canvas.getContext("2d");
@@ -20,16 +12,12 @@ export default function (req: VercelRequest, res: VercelResponse) {
   context.fillStyle = "#764abc";
   context.fillRect(0, 0, width, height);
 
-  // Set the style of the test and render it to the canvas
-  context.font = "bold 70pt 'PT Sans'";
+  context.font = "bold 40pt 'NotoSansJP'";
   context.textAlign = "center";
   context.fillStyle = "#fff";
-  // 600 is the x value (the center of the image)
-  // 170 is the y (the top of the line of text)
-  context.fillText(post.title, 600, 170);
-
-  const buffer = canvas.toBuffer("image/png");
+  context.fillText('宮本哲', 300, 200);
 
   res.setHeader('Content-Type', 'image/jpeg');
+  const buffer = canvas.toBuffer("image/png");
   res.send(buffer);
 }
