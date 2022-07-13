c394b80a3670a4c12cb54fbc675f1a3fdc4e4f82
Ohta Akira <waterlow3442@gmail.com>
2022-07-13 22:22:36 +0900

    画像を返してみる

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,10 @@
+import path from 'path';
+import fs from 'fs';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function (req: VercelRequest, res: VercelResponse) {
-  const { name = "World!" } = req.query;
-  res.send(`Hello ${name}!`);
+  const filePath = path.join(process.cwd(), `/api/150.jpeg`);
+  const imageBuffer = fs.readFileSync(filePath);
+  res.setHeader('Content-Type', 'image/jpeg');
+  res.send(imageBuffer);
 }
